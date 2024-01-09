f2256324723427d94d9a146b9925cf6d78d94359
Erica Ndong <ericandong6@gmail.com>
2024-01-09 15:37:21 +0100

    first component

--- office-frontend/src/app/page.js
+++ office-frontend/src/app/page.js
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import Logo from '../composants/logo'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      homepage
-    </main>
+    <main className="flex min-h-screen flex-col items-left justify-between p-24">
+    <header>
+      <Logo />
+    </header>
+    <section className="flex flex-col items-center">
+      <h1 className="text-3xl font-bold mb-4">Homepage</h1>
+      {/* Autres éléments de la page ici */}
+    </section>
+  </main>
   )
 }
