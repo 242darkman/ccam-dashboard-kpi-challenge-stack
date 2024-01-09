ddf1db5d8485d8062ef1b26b87682917cdbddfbf
Erica Ndong <ericandong6@gmail.com>
2024-01-09 17:20:44 +0100

    components folder

--- office-frontend/src/app/layout.js
+++ office-frontend/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import Head from 'next/head'; 
+import './globals.css';
+import logo from '../assets/logo.svg';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <header>
+          <div className="logo">
+            <img src={logo} alt="My App Logo" />
+          </div>
+        </header>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
-  )
+  );
 }
