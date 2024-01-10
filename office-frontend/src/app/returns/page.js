07756885d6c74771f63cbc418bb4e0ba4ecccb51
Red REMILI <red.remili@tuta.io>
2024-01-10 17:13:17 +0100

    add returnPage

---
+++ office-frontend/src/app/returns/page.js
@@ -0,0 +1,72 @@
+"use client";
+import { useState } from "react";
+
+
+export default function HoverRating() {
+    const [typeDemande, setTypeDemande] = useState("demande-1");
+    const [idLivraison, setIdLivraison] = useState("humidity-1");
+    const [message, setMessage] = useState("");
+
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24 bg-white">
+            <h1 className="text-[#22577a] font-['Arial'] text-4xl leading-[normal] mb-[60px]">
+                Retours et réclamation
+            </h1>
+            <div className="flex flex-col items-start">
+                {/* Type Demande */}
+                <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
+                    <div className="text-black font-['Roboto'] leading-[normal]">
+                        Spécifier le type de votre demande
+                    </div>
+                    <div className="flex items-start">
+                        <select
+                            value={typeDemande}
+                            onChange={(e) => setTypeDemande(e.target.value)}
+                            className="w-[350px] h-[40px] text-black bg-white border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600"
+                        >
+                            <option value="temperature-1">Correcte</option>
+                            <option value="temperature-2">Incorrecte</option>
+                        </select>
+                    </div>
+                </div>
+
+                {/* Humidité */}
+                <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
+                    <div className="text-black font-['Roboto'] leading-[normal]">
+                        Identifiant de la livraison
+                    </div>
+                    <div className="flex items-start">
+                        <select
+                            value={idLivraison}
+                            onChange={(e) => setIdLivraison(e.target.value)}
+                            className="w-[350px] h-[40px] text-black bg-white border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600"
+                        >
+                            <option value="humidity-1">Correcte</option>
+                            <option value="humidity-2">Incorrecte</option>
+                        </select>
+                    </div>
+                </div>
+
+                {/* Champ de texte long pour le message */}
+                <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
+                    <div className="text-black font-['Roboto'] leading-[normal]">
+                        Votre Message
+                    </div>
+                    <textarea
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="w-[100%] h-[120px] text-black bg-white border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:border-gray-600 resize-none"
+                    ></textarea>
+                </div>
+
+
+                <button
+                    type="button"
+                    className="w-full inline-flex justify-center items-center gap-2.5 p-2.5 p-2 rounded bg-[#57cc99] text-[#22577a] font-['Roboto'] font-medium leading-[normal]"
+                >
+                    Envoyer
+                </button>
+            </div>
+        </main>
+    );
+}
--- office-frontend/src/app/satisfaction/page.js
+++ office-frontend/src/app/satisfaction/page.js
@@ -1,11 +1,19 @@
 "use client";
 import { useState } from "react";
-import Stars from '@/components/stars';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
 
-export default function Home() {
+import Stack from '@mui/material/Stack';
+
+
+export default function HoverRating() {
     const [temperature, setTemperature] = useState("temperature-1");
     const [humidity, setHumidity] = useState("humidity-1");
     const [broken, setBroken] = useState("broken-1");
+
+    const [value, setValue] = React.useState(2);
+    const [value2, setValue2] = React.useState(1);
 
     return (
         <main className="flex min-h-screen flex-col items-center p-24 bg-white">
@@ -13,104 +21,6 @@ export default function Home() {
                 Votre avis
             </h1>
             <div className="flex flex-col items-start">
-                <div className="text-[#22577a] font-['Roboto'] text-2xl leading-[normal] font-medium mb-[45px]">
-                    État des produits
-                </div>
-                <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
-                    <div className="text-black font-['Roboto'] leading-[normal]">
-                        Température
-                    </div>
-                    <div className="flex items-start">
-                        <div className="flex items-center mr-[76px]">
-                            <div className="flex items-center mb-4">
-                                <input
-                                    id="temperature-1"
-                                    type="radio"
-                                    checked={temperature === "temperature-1"}
-                                    onChange={() => {
-                                        setTemperature("temperature-1");
-                                    }}
-                                    name="temperature"
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label
-                                    htmlFor="temperature-1"
-                                    className="text-black font-['Roboto'] leading-[normal] ms-2"
-                                >
-                                    Correcte
-                                </label>
-                            </div>
-                        </div>
-                        <div className="flex items-center">
-                            <div className="flex items-center mb-4">
-                                <input
-                                    id="temperature-2"
-                                    type="radio"
-                                    checked={temperature === "temperature-2"}
-                                    onChange={() => {
-                                        setTemperature("temperature-2");
-                                    }}
-                                    name="temperature"
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label
-                                    htmlFor="temperature-2"
-                                    className="text-black font-['Roboto'] leading-[normal] ms-2 "
-                                >
-                                    Incorrecte
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
-                    <div className="text-black font-['Roboto'] leading-[normal]">
-                        Humidité
-                    </div>
-                    <div className="flex items-start">
-                        <div className="flex items-center mr-[76px]">
-                            <div className="flex items-center mb-4">
-                                <input
-                                    id="humidity-1"
-                                    type="radio"
-                                    checked={humidity === "humidity-1"}
-                                    onChange={() => {
-                                        setHumidity("humidity-1");
-                                    }}
-                                    name="humidity"
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label
-                                    htmlFor="humidity-1"
-                                    className="text-black font-['Roboto'] leading-[normal] ms-2"
-                                >
-                                    Correcte
-                                </label>
-                            </div>
-                        </div>
-                        <div className="flex items-center">
-                            <div className="flex items-center mb-4">
-                                <input
-                                    id="humidity-2"
-                                    type="radio"
-                                    checked={humidity === "humidity-2"}
-                                    onChange={() => {
-                                        setHumidity("humidity-2");
-                                    }}
-                                    name="humidity"
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label
-                                    htmlFor="humidity-2"
-                                    className="text-black font-['Roboto'] leading-[normal] ms-2 "
-                                >
-                                    Incorrecte
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
                     <div className="text-black font-['Roboto'] leading-[normal]">
@@ -164,11 +74,30 @@ export default function Home() {
                     <div className="flex items-center text-black font-['Roboto'] leading-[normal]">
                         Évaluez votre expérience de livraison{" "}
                         <div className="flex items-start ml-[33px]">
-                            <Stars clicked />
-                            <Stars hovered />
-                            <Stars />
-                            <Stars />
-                            <Stars />
+                            <Stack spacing={1}>
+                                <Rating name="half-rating" defaultValue={1} precision={0.5} />
+                            </Stack>
+                        </div>
+                    </div>
+                </div>
+                <div className="shadow-md shadow-[#00000040] flex flex-col items-start gap-7 pt-[1.5625rem] pr-[1.5625rem] pb-[1.5625rem] pl-[1.5625rem] w-[774px] rounded-md bg-neutral-50 mb-[30px]">
+                    <div className="flex items-center text-black font-['Roboto'] leading-[normal]">
+                        À quel niveau nous recommander-vous ?{" "}
+                        <div className="flex items-start ml-[33px]">
+                            <Box
+                                sx={{
+                                    '& > legend': { mt: 2 },
+                                }}
+                            >
+                                <Rating
+                                    name="simple-controlled"
+                                    value={value2}
+                                    precision={0.5}
+                                    onChange={(event, newValue) => {
+                                        setValue2(newValue);
+                                    }}
+                                />
+                            </Box>
                         </div>
                     </div>
                 </div>
