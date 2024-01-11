528d29644b8af5b445865cb71bac01a23f9cf403
242darkman <53907600+242darkman@users.noreply.github.com>
2024-01-11 20:57:37 +0100

    fix: ajout des try/catch pour la gestion d'erreurs

--- core-backend/src/Controller/QuestionController.php
+++ core-backend/src/Controller/QuestionController.php
@@ -23,75 +23,83 @@ class QuestionController extends AbstractController
         QuestionRepository $questionRepository,
         Request $request
     ): JsonResponse {
-        // Récupérer les paramètres de pagination de la requête
-        $page = $request->query->get('page', 1); // Page par défaut est 1
-        $limit = $request->query->get('limit', 10); // Taille de la page par défaut
+        try {
+            // Récupérer les paramètres de pagination de la requête
+            $page = $request->query->get('page', 1); // Page par défaut est 1
+            $limit = $request->query->get('limit', 10); // Taille de la page par défaut
 
-        // Calculer l'offset
-        $offset = ($page - 1) * $limit;
+            // Calculer l'offset
+            $offset = ($page - 1) * $limit;
 
-        // Récupérer les questions avec pagination
-        $questions = $questionRepository->findBy([], null, $limit, $offset);
+            // Récupérer les questions avec pagination
+            $questions = $questionRepository->findBy([], null, $limit, $offset);
 
-        // Récupérer le nombre total de questions
-        $totalQuestions = $questionRepository->count([]);
-        $data = [];
+            // Récupérer le nombre total de questions
+            $totalQuestions = $questionRepository->count([]);
+            $data = [];
 
-        foreach ($questions as $question) {
-            $questionData = [
-                'id' => $question->getId(),
-                'description' => $question->getDescription(),
+            foreach ($questions as $question) {
+                $questionData = [
+                    'id' => $question->getId(),
+                    'description' => $question->getDescription(),
+                ];
+
+                $questionData['_links'] = [
+                    'self' => [
+                        'href' => $this->generateUrl('question_detail', ['id' => $question->getId()])
+                    ],
+                ];
+
+                $data[] = $questionData;
+            }
+
+            $paginationData = [
+                'currentPage' => $page,
+                'pageSize' => $limit,
+                'totalItems' => $totalQuestions,
+                'totalPages' => ceil($totalQuestions / $limit),
             ];
 
-            $questionData['_links'] = [
-                'self' => [
-                    'href' => $this->generateUrl('question_detail', ['id' => $question->getId()])
+            return $this->json(
+                [
+                    'questions' => $data,
+                    'pagination' => $paginationData
                 ],
-            ];
-
-            $data[] = $questionData;
+                Response::HTTP_OK
+            );
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $paginationData = [
-            'currentPage' => $page,
-            'pageSize' => $limit,
-            'totalItems' => $totalQuestions,
-            'totalPages' => ceil($totalQuestions / $limit),
-        ];
-
-        return $this->json(
-            [
-                'questions' => $data,
-                'pagination' => $paginationData
-            ],
-            Response::HTTP_OK
-        );
     }
 
 
     #[Route('/questions/get-questions/{id}', name: 'question_detail', methods: ['GET'])]
     public function getQuestionDetail(QuestionRepository $questionRepository, int $id): JsonResponse
     {
-        $question = $questionRepository->find($id);
+        try {
+            $question = $questionRepository->find($id);
 
-        if (!$question) {
+            if (!$question) {
+                return $this->json(
+                    ['message' => 'Question not found'],
+                    Response::HTTP_NOT_FOUND
+                );
+            }
+
+            $questionData = [
+                'id' => $question->getId(),
+                'description' => $question->getDescription(),
+            ];
+
             return $this->json(
-                ['message' => 'Question not found'],
-                Response::HTTP_NOT_FOUND
+                [
+                    'questions' => $questionData,
+                    'pagination' => []
+                ],
+                Response::HTTP_OK
             );
+        } catch (\Exception $e) {
+            return $this->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        $questionData = [
-            'id' => $question->getId(),
-            'description' => $question->getDescription(),
-        ];
-
-        return $this->json(
-            [
-                'questions' => $questionData,
-                'pagination' => []
-            ],
-            Response::HTTP_OK
-        );
     }
 }
